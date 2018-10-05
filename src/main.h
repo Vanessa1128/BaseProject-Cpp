222a91ae0a189d63976dc8ee07b6facbbb9a2570
Forrest-Li <jspzjnoms@gmail.com>
2018-10-05 20:07:43 +0800

    ADD header
    0

--- src/main.h
+++ src/main.h
@@ -3,5 +3,10 @@
 
 int addNumber(int a, int b);
 int getTriangleType(int a, int b, int c);
+<<<<<<< Updated upstream
 int check(int num);
+=======
+int sum(int x, int y, int z);
+
+>>>>>>> Stashed changes
 #endif // MAIN_H_INCLUDED
