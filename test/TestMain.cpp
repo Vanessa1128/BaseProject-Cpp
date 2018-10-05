5408a8132dd37dc3bc3598943cb1d9173042dbe1
Unknown <weiweimpnkey70113@gmail.com>
2018-10-05 20:02:40 +0800

    增加檢查
    對奇偶數檢查插入測試資料

--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -5,8 +5,13 @@ void testGetTriangleType() {
     assert(getTriangleType(3, 4, 5) == 0);
 }
 
+void testcheck() {
+    assert(check(2) == 0);
+}
+
 int main() {
     assert(addNumber(5, 2) == 7);
     testGetTriangleType();
+    testcheck();
     return 0;
 }
