0a11f5d7c90ad68213d8dffa832e2fb188afafac
Vanessa1128 <weiweimpnkey70113@gmail.com>
2018-12-26 23:19:27 +0800

    完成選單副程式  選擇功能

--- src/test.cpp
+++ src/test.cpp
@@ -1,38 +1,49 @@
 #include<stdio.h>
-
+#include<stdlib.h>
+#include<iostream>
 void show_menu();
 
 int main (void){
-	
-	int option; //功能選項 
-	
+
+	int option = 0; //功能選項
+
 	show_menu();//show選單
-	for(;;){
-		
-		if(scanf(" %d", & option) !=1 );
-		
+	for(;;){//無限迴圈直到輸入正確指令
+
+		if(scanf(" %d", & option) ==1 && option <=4 && option >= 1){//當輸入為整數且為功能選項時跳出
+            break;
+		}
+
+		else{//例外 顯示錯誤訊息 重新顯示選單 並清空輸入緩衝
+            printf("無效的指令!!");
+            system("pause");
+            system("CLS");
+            show_menu();
+            fflush(stdin);
+		}
 	}
-	
-	
+
+
 	if(option == 1){
-		
+
 	}
 	else if(option == 2){
-		
+
 	}
 	else if(option == 3){
-		
+
 	}
-	else{
-		
+	else if(option == 4){
+
 	}
+
 	//switch寫法 對應各功能
 	//1.login
 	//2.create account
 	//3.quit
 	//4.改密碼
 
-
+    return 0;
 }
 
 ///////////////////////////////////////////////
@@ -43,7 +54,7 @@ int main (void){
 void show_menu() {
     printf("<<歡迎使用找室友系統>>\n");
     printf("按下數字鍵選擇服務\n");
-    printf("1.登入  2.新建帳號 3.離開系統\n");
+    printf("1.登入  2.新建帳號 3.修改密碼 4.離開系統\n");
     printf("請輸入服務選項:");
 }
 
